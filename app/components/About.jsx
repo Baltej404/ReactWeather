a54a563aa9c74d713362dd8788995e9968379e96
Baltej <Baltej>
2016-07-11 15:39:26 +0100

    updated about page with links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,22 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>Welcome To About</h3>
+    <div>
+      <h1 className="text-center">Welcome To About</h1>
+        <p>This is the about us page and will give you the temperture of a given location</p>
+        <p>Here are some of the tools to use:</p>
+        <ul>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - this was the
+              javascript framework used.
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open weather map</a> - I used
+              the openweathermap api for the temperture.
+          </li>
+        </ul>
+    </div>
+
   )
 };
 
